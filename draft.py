d76194cb3c71a8e69032a5a8d63aee47fb022ca2
arnab125 <arnabtasnimpranto@gmail.com>
2022-11-27 02:11:39 +0600

    final

--- draft.py
+++ draft.py
@@ -1,17 +1,4 @@
-"""a=ord("A")
-print(a)
-b=chr(65)
-print(b)"""
-"""def convert(s):
-    listed = []
-    tupled = ()
-    if s[0].isalpha():
-        tupled=(*tupled,str(ord(s[0].upper())-65),s[1])
-        listed.append(tupled)
-    else:
-        print("Invalid Seat Number")
-    return listed
-print(convert("a25"))"""
+
 
 def convert(s):
     listed = []
